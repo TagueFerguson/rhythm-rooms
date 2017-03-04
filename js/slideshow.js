1793a54f5d02dd89ac531843f66a68f6d02e19c6
Tague Ferguson <tagueferguson@live.co.uk>
2017-03-04 12:57:45 +0000

    Remove absolute positioning on footer, changes images size and sets arrows to absolute in the middle of the picture

---
+++ js/slideshow.js
@@ -0,0 +1,17 @@
+var slideIndex = 1;
+
+function plusSlides (n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides (n) {
+  var slides = document.getElementsByClassName('slides');
+  if (n > slides.length) slideIndex = 1;
+  if (n < 1) slideIndex = slides.length;
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slides[slideIndex - 1].style.display = 'block';
+}
+
+showSlides(slideIndex);
